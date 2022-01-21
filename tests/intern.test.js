6cb64ab15b8a583b9a6f41efbbae2d84cd2b129e
cmseibel <cam.m.seib@gmail.com>
2022-01-21 11:31:18 -0800

    Write unit tests for Intern class.

---
+++ tests/intern.test.js
@@ -0,0 +1,24 @@
+const { describe, test, expect } = require("@jest/globals");
+const Intern = require("../lib/intern");
+
+describe("constructor", () => {
+    test("should have a school property", () => {
+        new intern = Intern("Jed", "jed@hmail.net", "For-Profit Online University");
+        expect(intern.school).toBeTruthy();
+    });
+});
+describe("functions", () => {
+    describe("getSchool", () => {
+        test("should return the intern's school", () => {
+            let school = "For-Profit Online University"
+            new intern = Intern("Jed", "jed@hmail.net", school);
+            expect(intern.getSchool()).toEqual(school);
+        });
+    })
+    describe("getRole", () => {
+        test("should return the 'Intern' role", () => {
+            new intern = Intern("Jed", "jed@hmail.net", "For-Profit Online University");
+            expect(intern.getRole()).toEqual("Intern");
+        });
+    });
+});
