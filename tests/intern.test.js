9747a158da6c3b0d81642d065a3dc53fef9895b5
cmseibel <cam.m.seib@gmail.com>
2022-01-21 12:04:49 -0800

    Fix bugs in intern unit tests.

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -2,7 +2,7 @@ const Intern = require("../lib/intern");
 
 describe("constructor", () => {
     test("should have a school property", () => {
-        new intern = Intern("Jed", "jed@hmail.net", "For-Profit Online University");
+        let intern = new Intern("Jed", "jed@hmail.net", "For-Profit Online University");
         expect(intern.school).toBeTruthy();
     });
 });
@@ -10,13 +10,13 @@ describe("functions", () => {
     describe("getSchool", () => {
         test("should return the intern's school", () => {
             let school = "For-Profit Online University"
-            new intern = Intern("Jed", "jed@hmail.net", school);
+            let intern = new Intern("Jed", "jed@hmail.net", school);
             expect(intern.getSchool()).toEqual(school);
         });
     })
     describe("getRole", () => {
         test("should return the 'Intern' role", () => {
-            new intern = Intern("Jed", "jed@hmail.net", "For-Profit Online University");
+            let intern = new Intern("Jed", "jed@hmail.net", "For-Profit Online University");
             expect(intern.getRole()).toEqual("Intern");
         });
     });
